f9992dc6b48e66bae0a61351bd4f44cb8cfbeae8
bky113344 <bky113344@gmail.com>
2026-05-11 07:55:20 +0000

    practice

--- src/main/java/com/leetcodeblind75/DetectCycleInLinkedList.java
+++ src/main/java/com/leetcodeblind75/DetectCycleInLinkedList.java
@@ -41,7 +41,7 @@ public class DetectCycleInLinkedList {
 
         while(fast != null && fast.next != null){
              slow = slow.next;
-             fast = fast.next;
+             fast = fast.next.next;
              if(slow==fast)
                 return true ;
         }
